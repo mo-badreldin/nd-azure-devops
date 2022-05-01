37eed1b3795f38ff03acddd10547638ee8a496bf
mo-badreldin <badreldin1990@gmail.com>
2022-05-01 18:51:29 +0200

    break the build

--- Makefile
+++ Makefile
@@ -5,9 +5,15 @@ install:
 	pip install --upgrade pip &&\
 		pip install -r requirements.txt
 
+lint:
+	pylint --disable=R,C,E1120 hello.py
+
 test_adder:
 	python -m pytest -vv test_adder.py
 
 test_hello:
 	python -m pytest -vv test_hello.py
+
+exec:
+	python hello.py
 all: hello install
--- hello.py
+++ hello.py
@@ -1,12 +1,13 @@
 import click
 
+var=
 @click.command(help=="This is just a hello app, that does nothing")
 @click.option("--name",prompt="I need name", help="Need name")
 @click.option("--color",prompt="I need color", help="Need color")
 
 def hello_tool(name, color):
     if name == "Thor":
-        click.echo("Thor, your are always red.")
+        click.echo(f"Thor, your are not {color}, you are always red.")
         click.echo(click.style(f"Hello {name}!",fg="red"))
     else:
         #click.echo(click.style(f"Hello {name}!",fg=f"{color}"))
@@ -14,4 +15,4 @@ def hello_tool(name, color):
         click.echo(click.style(f"Hello {name}!", fg=color))
 
 if __name__ == "__main__":
-    hello()
+    hello_tool()
